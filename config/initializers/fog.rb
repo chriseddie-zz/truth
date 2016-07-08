5d115372a47cec50ec128fb4ab0bfc0e9c5930e3
Chris Eddie <hello@chriseddie.com>
2016-07-07 22:20:01 -0700

    removed access tokens

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,9 +1,9 @@
-  # 
+  #
   # CarrierWave.configure do |config|
   #   config.fog_credentials = {
   #     provider:               'AWS',
-  #     aws_access_key_id:      ENV['AKIAI3TYFGMI6TFRB3MA'],
-  #     aws_secret_access_key:  ENV['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'],
+  #     aws_access_key_id:      ENV[''],
+  #     aws_secret_access_key:  ENV[''],
   #   }
   #
   #   # For testing, upload files to local `tmp` folder.
@@ -23,11 +23,12 @@
   CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider               => 'AWS',       # required
-      :aws_access_key_id      => 'AKIAI3TYFGMI6TFRB3MA',       # required
-      :aws_secret_access_key  => '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',       # required
-      # :region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
+      :aws_access_key_id      => '',       # required
+      :aws_secret_access_key  => '',       # required
+      :region                 => 'us-west-2'  # optional, defaults to 'us-east-1'
     }
     config.fog_directory  = 'truthdrums-drums'                     # required
+    config.cache_dir        = "#{Rails.root}/tmp/uploads" # To let CarrierWave work on Heroku
     # config.fog_host       = 'https://assets.example.com'            # optional, defaults to nil
     # config.fog_public     = false                                   # optional, defaults to true
     # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
