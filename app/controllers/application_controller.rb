11a10552ed897ca0443b9e0bda27d82faed79c00
chriseddie <hello@chriseddie.com>
2017-03-06 21:50:06 -0800

    Remove auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,20 +8,4 @@ class ApplicationController < ActionController::Base
     @site_content = Content.find_by_content_key("key_header_img")
   end
 
-  # before_action :set_is_logged_in
-  #
-  # def authenticate
-  #   authenticate_or_request_with_http_basic do |user_name, password|
-  #     if (user_name == "admin" && password == "pass")
-  # 		session[:is_logged_in] = "1"
-  # 		return true
-  # 	else
-  # 		return false
-  # 	end
-  #   end
-  # end
-  #
-  # def set_is_logged_in
-  #   @is_logged_in = session[:is_logged_in]
-  # end
 end
