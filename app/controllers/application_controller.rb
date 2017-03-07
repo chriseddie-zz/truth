7750dbd4f8825127503f82865822b6007260c776
Jason Cave <jcave@cbtnuggets.com>
2017-03-07 13:42:48 -0800

    Fixed the header shit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
   before_action :set_site_variables
+
   def set_site_variables
-    @site_content = Content.find_by_content_key('key_header_img')
+    content = Content.find_by_content_key('key_header_img')
+    @site_content = content ? content.content_text : ""
   end
 
 end
